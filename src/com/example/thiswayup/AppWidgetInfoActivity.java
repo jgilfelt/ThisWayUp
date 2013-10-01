2bec80ed7a39cea9a3eb7612a31149f3886784f7
jgilfelt <jgilfelt@gmail.com>
2013-10-01 20:12:41 +0100

    added app widget info activity

---
+++ src/com/example/thiswayup/AppWidgetInfoActivity.java
@@ -0,0 +1,26 @@
+package com.example.thiswayup;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
+
+public class AppWidgetInfoActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_app_widget_info);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+}
